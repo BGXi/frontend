f1db5fb7ffeafe12e9199d7eb854ee41db920b69
deg developer <deg@rt.ru>
2020-09-07 14:29:09 +0300

    first commit

---
+++ projects/voting-box/src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  electionsUrl: '//e-vote.dev.gas20.ru/ballot-box/',
+  sentryDsn: 'https://9a3e68021a124e3887d5f35329e9c8e7@sentry.dev.gas20.ru/4',
+  name: 'develop',
+  showSha: false,
+};
---
+++ projects/voting-box/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  electionsUrl: '//deg.rt.ru/ballot-box/',
+  sentryDsn: 'https://e8e855a045ce4626a26c502d2f588d69@sentry.dev.gas20.ru/7',
+  name: 'production',
+  showSha: false,
+};
